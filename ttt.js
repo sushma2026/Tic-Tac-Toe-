dc32e8265f8ac72cb045036bbc05e6a416c75b04
sushma pagadala <88230359+sushma2026@users.noreply.github.com>
2022-10-29 10:31:30 +0530

    Update ttt.js

--- ttt.js
+++ ttt.js
@@ -4,48 +4,71 @@ function f1(){
 
 if(flag==1){
 document.getElementById("s").value="x";
+document.getElementById("s").disabled=true;
 flag=0;}
-else{document.getElementById("s").value="0";flag=1;}
+else{document.getElementById("s").value="0";}
+document.getElementById("s").disabled=true;
+flag=1;
 }
 function f2(){
 
-if(flag==1){document.getElementById("s1").value="x";flag=0;}
-else{document.getElementById("s1").value="0";flag=1;}
+if(flag==1){document.getElementById("s1").value="x";
+document.getElementById("s1").disabled=true;flag=0;}
+else{document.getElementById("s1").value="0";
+document.getElementById("s1").disabled=true;flag=1;}
 }
 function f3(){
 
-if(flag==1){document.getElementById("s2").value="x";flag=0;}
-else{document.getElementById("s2").value="0";flag=1;}
+if(flag==1){document.getElementById("s2").value="x";
+document.getElementById("s2").disabled=true;flag=0;}
+else{document.getElementById("s2").value="0";
+document.getElementById("s2").disabled=true;
+flag=1;}
 }
 function f4(){
 
-if(flag==1){document.getElementById("s3").value="x";flag=0;}
-else{document.getElementById("s3").value="0";flag=1;}
+if(flag==1){document.getElementById("s3").value="x";
+document.getElementById("s3").disabled=true;flag=0;}
+else{document.getElementById("s3").value="0";
+document.getElementById("s3").disabled=true;
+flag=1;}
 }
 function f5(){
 
-if(flag==1){document.getElementById("s4").value="x";flag=0;}
-else{document.getElementById("s4").value="0";flag=1;}
+if(flag==1){document.getElementById("s4").value="x";
+document.getElementById("s4").disabled=true;flag=0;}
+else{document.getElementById("s4").value="0";
+document.getElementById("s4").disabled=true;
+flag=1;}
 }
 function f6(){
 
-if(flag==1){document.getElementById("s5").value="x";flag=0;}
-else{document.getElementById("s5").value="0";flag=1;}
+if(flag==1){document.getElementById("s5").value="x";
+document.getElementById("s5").disabled=true;flag=0;}
+else{document.getElementById("s5").value="0";
+document.getElementById("s5").disabled=true;
+flag=1;}
 }
 function f7(){
 
-if(flag==1){document.getElementById("s6").value="x";flag=0;}
-else{document.getElementById("s6").value="0";flag=1;}
+if(flag==1){document.getElementById("s6").value="x";document.getElementById("s6").disabled=true;
+flag=0;}
+else{document.getElementById("s6").value="0";
+document.getElementById("s6").disabled=true;flag=1;}
 }
 function f8(){
 
-if(flag==1){document.getElementById("s7").value="x";flag=0;}
-else{document.getElementById("s7").value="0";flag=1;}
+if(flag==1){document.getElementById("s7").value="x";
+document.getElementById("s7").disabled=true;flag=0;}
+else{document.getElementById("s7").value="0";
+document.getElementById("s7").disabled=true;flag=1;}
 }
 function f9(){
 
-if(flag==1){document.getElementById("s8").value="x";flag=0;}
-else{document.getElementById("s8").value="0";flag=1;}
+if(flag==1){document.getElementById("s8").value="x";
+document.getElementById("s8").disabled=true;flag=0;}
+else{document.getElementById("s8").value="0";
+document.getElementById("s8").disabled=true;flag=1;}
 }
 function log(){
 s=document.getElementById("s").value;
